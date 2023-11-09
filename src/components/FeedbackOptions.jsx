6caab08268485c9b7cc6aa0548012938a1db45b1
Yuriy Apanasenko <gepa9b@gmail.com>
2023-11-09 19:57:00 +0200

    homework ready

---
+++ src/components/FeedbackOptions.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import shortid from 'shortid';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <>
+      {options.map(option => (
+        <button
+          key={shortid.generate()}
+          type="button"
+          name={option}
+          onClick={onLeaveFeedback}
+        >
+          {option}
+        </button>
+      ))}
+    </>
+  );
+};
+export default FeedbackOptions;
